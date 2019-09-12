ae7853f487c1a1b4ab12eea8c58bce2502c3a183
Sara Coren <saracoren@Saras-MacBook-Air.local>
2019-09-12 12:11:58 -0400

    made buttons and textarea

---
+++ client/src/Wordbank.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './wordbank.css';
+
+function Wordbank() {
+    return (
+<>
+<button>Tired</button>
+<button>Anxious</button>
+<button>Excited</button>
+<button>Bored</button>
+<button>Hungry</button>
+<h1>Optional: Is there anything else you would like your teacher to know today?</h1>
+<textarea></textarea>
+<button>Submit</button>
+</>
+    )
+}
+
+export default Wordbank;
